a38414af9523f94e0d2f9e59459621ad5980d669
langtoi <langtoi.bk@gmail.com>
2023-05-01 08:08:40 +0700

    commit message

---
+++ randomedomain.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+def tao_domain_ngau_nhien(base_domain, length=3, limit=20):
+    for _ in range(limit):
+        random_chars = ''.join(random.choices(string.ascii_lowercase, k=length))
+        new_domain = base_domain.replace('xxx', random_chars)
+        print(new_domain)
+
+base_domain = 'mygiftcardmallxxx.com'
+tao_domain_ngau_nhien(base_domain)
