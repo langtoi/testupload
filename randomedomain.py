964c7a2be1e3d09a20fb34db6b351a46c61ecc14
langtoi <langtoi.bk@gmail.com>
2023-05-01 09:00:11 +0700

    update

--- randomedomain.py
+++ randomedomain.py
@@ -1,7 +1,7 @@
 import random
 import string
 
-def tao_domain_ngau_nhien(base_domain, length=3, limit=20):
+def tao_domain_ngau_nhien(base_domain, length=3, limit=2000):
     for _ in range(limit):
         random_chars = ''.join(random.choices(string.ascii_lowercase, k=length))
         new_domain = base_domain.replace('xxx', random_chars)
